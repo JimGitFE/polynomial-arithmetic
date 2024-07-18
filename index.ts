318072af39a5093f30bfe1e8d89993267d5ed847
JimGitFE <rymsjfjrys@gmail.com>
2024-07-17 23:57:48 -0300

    feat: Centralize ALL exports in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
-
+// Arithmetic
 import { Polynomial } from "./arithmetic"
 import { FieldPolynomial } from "./galois-field-arithmetic"
 
-const utils = {
-    title: "Internal usage math utilities",
+// Utils
+import { removeLZero, polyReformat } from "./utils/formatter"
+import { gcd, lcm, arrayGcd, eea, isAllZero, polyGcd} from "./utils/gcd"
+import { isPrime, powerMod, mod } from "./utils/math"
+import { filterDuplicates, arrayMax } from "./utils/polynomial"
 
+/**
+ * Internal usage math & formatting utilities
+*/
+const utils = {
+    removeLZero, polyReformat,
+    gcd, lcm, arrayGcd, eea, isAllZero, polyGcd,
+    isPrime, powerMod, mod,
+    filterDuplicates, arrayMax
 }
 
 export { Polynomial, FieldPolynomial, utils }
