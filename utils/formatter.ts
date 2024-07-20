6f598395107fcf40c287348aeaa15a50d014e38d
JimGitFE <rymsjfjrys@gmail.com>
2024-07-20 04:31:09 -0300

    Refactor: Use new types from global.d.ts in formatter.ts

--- utils/formatter.ts
+++ utils/formatter.ts
@@ -7,20 +7,20 @@ Coefficients: Array of polynomial term coefficients, x^2 + 1 => [1, 0, 1]
 note: polynomials in GF(2), have coefficients 0 or 1
 
 */
-import { PolynomialParameters, PolynomialFormats } from '../glboal';
+import { PolynomialParameters, polyString, polyCoefficients, polyExponents } from '../glboal';
 import { Polynomial } from '../arithmetic';
 
 /** 
  * Formats from array of coefficients to polynomial String.
  * 
- * @param { number[] } coefs - Polynomial to format. Example: [1,1,1,0,1,1,0,0,1,1]
- * @returns {string} - The formatted polynomial. Example: x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1  
+ * @param { polyCoefficients } coefs - Polynomial to format. Example: [1,1,1,0,1,1,0,0,1,1]
+ * @returns { polyString } - The formatted polynomial. Example: "x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1" 
  * @example
  * const formatted = coefToString([1,1,1,0,1,1,0,0,1,1]);
- * console.log(formatted); // Output: x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1
+ * console.log(formatted); // Output: "x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1"
  * @internal
 */
-export function coefToString (coefs: number[]): string {
+export function coefToString (coefs: polyCoefficients): polyString {
     // Map each coefficient to its term
     let terms = coefs.map((coef, i) => {
         if (coef === 0) {
@@ -42,16 +42,16 @@ export function coefToString (coefs: number[]): string {
 /**
  * Formats from polynomial String to array of coefficients.
  *  
- * @param { number[] } coefs - Polynomial to format. Example: x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1 
- * @returns { number[] } - The formatted polynomial. Example: [1,1,1,0,1,1,0,0,1,1]  
+ * @param { polyString } polyStr - Polynomial to format. Example: "x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1 "
+ * @returns { polyCoefficients } - The formatted polynomial. Example: [1,1,1,0,1,1,0,0,1,1]  
  * @example
- * const formatted = stringToCoef(x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1);
+ * const formatted = stringToCoef("x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1");
  * console.log(formatted) // Output: [1,1,1,0,1,1,0,0,1,1]
  * @internal
  */
-export const stringToCoef = (coefs: string): number[] => {
+export const stringToCoef = (polyStr: polyString): polyCoefficients => {
     // 1.2 Split the polynomial into terms
-    let terms = coefs.split(/(?=[+-])/g);
+    let terms = polyStr.split(/(?=[+-])/g);
 
     // 1.3 Find the highest exponent
     let highestExponent = Math.max(...terms.map(term => parseInt(term.split('^')[1]) || (term.includes("x")?1:0)));
@@ -75,14 +75,14 @@ export const stringToCoef = (coefs: string): number[] => {
 /** 
  * Formats from array of exponents to array of coefficients.
  * 
- * @param { number[] } exponentArr - Polynomial to format. Example: [9,8,7,5,4,1,0]
- * @returns { (0 | 1|-1)[] } - The formatted polynomial. Example: [1,1,1,0,1,1,0,0,1,1]  
+ * @param { polyExponents } exponentArr - Polynomial to format. Example: [9,8,7,5,4,1,0]
+ * @returns { polyCoefficients } - The formatted polynomial. Example: [1,1,1,0,1,1,0,0,1,1]  
  * @example
  * const formatted = expToCoef([9,8,7,5,4,1,0]);
  * console.log(formatted) // Output: [1,1,1,0,1,1,0,0,1,1]
  * @internal
 */
-export const expToCoef = (exponentArr: number[]): (0 | 1|-1)[] => {
+export const expToCoef = (exponentArr: polyExponents): polyCoefficients => {
     //  1.1 Define arrays with length = polynomial degree + 1
     let coefArr = new Array(Math.abs(Math.max(...exponentArr))+1).fill(0)
 
@@ -101,23 +101,23 @@ export const expToCoef = (exponentArr: number[]): (0 | 1|-1)[] => {
 /** 
  * Formats from array of coefficients to array of exponents.
  * 
- * @param { number[] } coefs - Polynomial to format. Example: [1,1,1,0,1,1,0,0,1,1]
- * @returns { number[] } - The formatted polynomial. Example: [9,8,7,5,4,1,0]  
+ * @param { polyCoefficients } coefArr - Polynomial to format. Example: [1,1,1,0,1,1,0,0,1,1]
+ * @returns { polyExponents } - The formatted polynomial. Example: [9,8,7,5,4,1,0]  
  * @example
  * const formatted = coefToExp([1,1,1,0,1,1,0,0,1,1]);
  * console.log(formatted) // Output: [9,8,7,5,4,1,0]
  * @internal
 */
-export const coefToExp = (coefArr: (0 | 1|-1)[]): number[] => {
+export const coefToExp = (coefArr: polyCoefficients): polyExponents => {
 
-    return coefArr.reduce((all: number[],num,i,arr)=>num===1?[...all, (arr.length - i - 1)]:all,[])
+    return coefArr.reduce((all: polyCoefficients, num, i, arr) => num===1?[...all, (arr.length - i - 1)]:all,[])
 }
 
 /** 
  * Removes leading zeroes from array. 
  * 
  * @param { number[] } poly0 - Input Array. Example: [0,1]
- * @returns {  } - Result Array, Example: [1]  
+ * @returns { number[] | (0|1|-1)[] } - Result Array, Example: [1]  
  * @example
  * const formatted = removeLZero([0,1]);
  * console.log(formatted) // Output: [1]
@@ -134,14 +134,14 @@ export const removeLZero = (poly0: number[]): number[] | (0|1|-1)[] => {
  * Translates a polynomial of any format into all representations.
  *
  * @param {PolynomialParameters} poly - The input polynomial. It can be an instance of the Polynomial class, a string representation, an exponent array representation, or a coefficient array representation.
- * @returns {PolynomialFormats} - An object containing the polynomial in all formats: string, coefficients array, and exponents array.
+ * @returns {[polyString, polyCoefficients, exponents]} - An object containing the polynomial in all formats: string, coefficients array, and exponents array.
  * @throws {Error} - Throws an error if the input format is invalid.
  * @example
  * const result = polyReformat('x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1');
  * console.log(result); // Output: {polynomialString: 'x^9 + x^8 + x^7 + x^5 + x^4 + x^1 + 1', coefficients: [1,1,1,0,1,1,0,0,1,1], exponents: [9,8,7,5,4,1,0]}
  * @public
  */
-export const polyReformat = (poly: PolynomialParameters ): PolynomialFormats => {
+export const polyReformat = (poly: PolynomialParameters ): [polyString, polyCoefficients, polyExponents] => {
 
     // Polynomial Class // [1,0] => [1,0] 
 if (poly instanceof Polynomial) {
